d318fa773500e74d911d56acfe542a4f25617a2f
shadow <81448108+shdwmtr@users.noreply.github.com>
2024-12-10 00:28:02 -0400

    fix: Update metadata generator

--- scripts/release.js
+++ scripts/release.js
@@ -2,31 +2,37 @@ const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
-module.exports = {
-  generateNotes: async () => {
-    const pluginsDir = path.resolve(process.cwd(), 'plugins');
-    let releaseNotes = `# Submodule Commit IDs\n\n`;
+const ParsePlugin = () => {
+    const submodulePath = path.join(pluginsDir, submodule);
 
-    if (fs.existsSync(pluginsDir)) {
-      const submodules = fs.readdirSync(pluginsDir);
-
-      submodules.forEach(submodule => {
-        const submodulePath = path.join(pluginsDir, submodule);
-        if (fs.existsSync(path.join(submodulePath, '.git'))) {
-          try {
-            const commitId = execSync(`git -C ${submodulePath} rev-parse HEAD`, { encoding: 'utf-8' }).trim();
-            releaseNotes += `- **${submodule}**: ${commitId}\n`;
-          } catch (error) {
-            releaseNotes += `- **${submodule}**: Error fetching commit ID\n`;
-          }
-        } else {
-          releaseNotes += `- **${submodule}**: Not a Git repository\n`;
+    if (fs.existsSync(path.join(submodulePath, '.git'))) {
+        try {
+            return { plugin: submodule, commitId: execSync(`git -C ${submodulePath} rev-parse HEAD`, { encoding: 'utf-8' }).trim() };
+        } 
+        catch (error) {
+            return { plugin: submodule, commitId: null };
         }
-      });
-    } else {
-      releaseNotes += `No plugins directory found.\n`;
+    } 
+    else {
+        return { plugin: submodule, commitId: null };
     }
+}
 
-    return releaseNotes;
-  }
+
+module.exports = {
+    generateNotes: async () => {
+        const pluginsDir = path.resolve(process.cwd(), 'plugins');
+        let pluginIds = [];
+        let releaseNotes = '';
+
+        if (fs.existsSync(pluginsDir)) {
+            fs.readdirSync(pluginsDir).forEach(submodule => { pluginIds.push(ParsePlugin(submodule)); });
+        } 
+
+        for (let plugin of pluginIds) {
+            releaseNotes += `${btoa(plugin.plugin)}: ${plugin.commitId}\n`;
+        }
+
+        return releaseNotes;
+    }
 };
