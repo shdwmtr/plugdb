a211bc33bd3c7b6247cb01e605a36140d75a1381
shadow <81448108+shdwmtr@users.noreply.github.com>
2024-12-10 00:42:47 -0400

    fix: Ensure static plugin names

--- scripts/release.js
+++ scripts/release.js
@@ -2,19 +2,35 @@ const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
+const GetPluginName = (pluginDir) => { 
+    const jsonPath = path.join(pluginDir, 'plugin.json');
+
+    try {
+        if (fs.existsSync(jsonPath)) {
+            const pluginJson = JSON.parse(fs.readFileSync(jsonPath));
+            return pluginJson.name;
+        }
+    }
+    catch (error) {
+        return null;
+    }
+}
+
 const ParsePlugin = (pluginsDir, submodule) => {
     const submodulePath = path.join(pluginsDir, submodule);
 
+    const pluginName = execSync(`git -C ${submodulePath} rev-list --max-parents=0 HEAD`, { encoding: 'utf-8' }).trim();
+
     if (fs.existsSync(path.join(submodulePath, '.git'))) {
         try {
-            return { plugin: submodule, commitId: execSync(`git -C ${submodulePath} rev-parse HEAD`, { encoding: 'utf-8' }).trim() };
+            return { plugin: pluginName, commitId: execSync(`git -C ${submodulePath} rev-parse HEAD`, { encoding: 'utf-8' }).trim() };
         } 
         catch (error) {
-            return { plugin: submodule, commitId: null };
+            return { plugin: pluginName, commitId: null };
         }
     } 
     else {
-        return { plugin: submodule, commitId: null };
+        return { plugin: pluginName, commitId: null };
     }
 }
 
