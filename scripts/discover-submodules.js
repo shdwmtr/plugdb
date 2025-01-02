1f478ba60613ffbc568855e9bd2357d0c43ad1ea
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-01-01 20:47:56 -0400

    Update discover-submodules.js

--- scripts/discover-submodules.js
+++ scripts/discover-submodules.js
@@ -1,26 +1,25 @@
 const { execSync } = require('child_process');
 
-// log current directory
-// console.log('Current directory:', execSync('pwd').toString().trim());
 let json = [];
 
 try {
-    const submoduleNames = execSync('git submodule foreach --quiet "echo \$name"').toString().split('\n').filter(Boolean);
-    console.log('submoduleNames:', submoduleNames)
+    const submodules = execSync('git submodule')
+        .toString()
+        .split('\n')
+        .filter(Boolean);
 
-    const topLevelDir = execSync('git rev-parse --show-toplevel').toString().trim();
-    console.log('topLevelDir:', topLevelDir)
+    console.log("submodules", submodules);
 
-    submoduleNames.forEach((name) => {
-        const url = execSync(`git config --file ${topLevelDir}/.gitmodules submodule.${name}.url`).toString().trim();
-        
+    submodules.forEach((line) => {
+        const [, , name] = line.split(' ');
+        const url = execSync(`git config --get submodule.${name}.url`).toString().trim();
+
         const owner = url.replace(/https:\/\/github.com\/([^\/]+)\/.*/, '$1');
         const repo = url.replace(/https:\/\/github.com\/[^\/]+\/([^\/]+).*/, '$1').replace(/\.git$/, '');
-        
+
         json.push({ owner: owner, repo: repo });
     });
-} 
-catch (error) {
+} catch (error) {
     console.error('Error processing submodules:', error);
 }
 
