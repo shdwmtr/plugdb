10da777bd48202d73935343cef99aca001dfd830
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-01-01 20:25:54 -0400

    fix: Fix CI

---
+++ scripts/discover-submodules.js
@@ -0,0 +1,23 @@
+const { execSync } = require('child_process');
+const fs = require('fs');
+
+let json = [];
+
+try {
+    const submoduleNames = execSync('git submodule foreach --quiet "echo $name"').toString().split('\n').filter(Boolean);
+    const topLevelDir = execSync('git rev-parse --show-toplevel').toString().trim();
+
+    submoduleNames.forEach((name) => {
+        const url = execSync(`git config --file ${topLevelDir}/.gitmodules submodule.${name}.url`).toString().trim();
+        
+        const owner = url.replace(/https:\/\/github.com\/([^\/]+)\/.*/, '$1');
+        const repo = url.replace(/https:\/\/github.com\/[^\/]+\/([^\/]+).*/, '$1').replace(/\.git$/, '');
+        
+        json.push({ owner: owner, repo: repo });
+    });
+} 
+catch (error) {
+    console.error('Error processing submodules:', error);
+}
+
+fs.writeFileSync('submodules.json', JSON.stringify(json, null, 4));
