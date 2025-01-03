9c19665ea33e9791dc01d0a1bf8eb0b77a8c9a10
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-01-03 03:16:44 -0400

    Update generate-metadata.js

--- scripts/generate-metadata.js
+++ scripts/generate-metadata.js
@@ -1,21 +1,11 @@
 const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
-const GetPluginName = (pluginDir) => { 
-    const jsonPath = path.join(pluginDir, 'plugin.json');
-    try {
-        if (fs.existsSync(jsonPath)) {
-            const pluginJson = JSON.parse(fs.readFileSync(jsonPath));
-            return pluginJson.name;
-        }
-    }
-    catch (error) {
-        return null;
-    }
-}
+
 const ParsePlugin = (pluginsDir, submodule) => {
     const submodulePath = path.join(pluginsDir, submodule);
     const pluginName = execSync(`git -C ${submodulePath} rev-list --max-parents=0 HEAD`, { encoding: 'utf-8' }).trim();
+    
     if (fs.existsSync(path.join(submodulePath, '.git'))) {
         try {
             return { id: pluginName, commitId: execSync(`git -C ${submodulePath} rev-parse HEAD`, { encoding: 'utf-8' }).trim() };
@@ -28,8 +18,10 @@ const ParsePlugin = (pluginsDir, submodule) => {
         return { id: pluginName, commitId: null };
     }
 }
+
 const pluginsDir = path.resolve(process.cwd(), 'plugins');
 let pluginIds = [];
+
 if (fs.existsSync(pluginsDir)) {
     fs.readdirSync(pluginsDir).forEach(submodule => { pluginIds.push(ParsePlugin(pluginsDir, submodule)); });
 } 
