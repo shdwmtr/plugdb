9fd9306c4a90212cac253b03f178dcc4f61c69a0
Boss Sloth <tddebart@gmail.com>
2025-02-28 12:35:09 +0100

    fix: ci

--- scripts/build/upload-dist.sh
+++ scripts/build/upload-dist.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "::group::Database"
 
+PLUGIN_NAME=$1
+
 cd dist
 
 echo "::debug::Building plugin archive..."
@@ -8,7 +10,7 @@ zip -r "$PLUGIN_NAME.zip" .
 
 echo "::debug::Successfully built plugin."
 
-id=$(jq -r '.id' "$PLUGIN_NAME"/metadata.json)
+id=$(jq -r '.id' "$PLUGIN_NAME"/metadata.json) || { echo "::error::Failed to extract id from $PLUGIN_NAME/metadata.json"; exit 1; }
 
 echo "::debug::Uploading plugin to database..."
 gsutil cp "$PLUGIN_NAME.zip" gs://millennium-d9ce0.appspot.com/plugins/"$id.zip"
