6c9dbba131038cdf0bd3252078c8ce7e1d48ebc7
BossSloth <44511414+BossSloth@users.noreply.github.com>
2025-02-28 01:24:06 +0100

    refactor: split CI shell scripts into separate files for reusability/readability, put plugins into own directory in zip and caching for database SDK (#23)
    * chore: split CI shell scripts into separate files for reusability and put plugins into own directory in zip
    
    * chore: try caching database sdk
    
    * chore: comment database upload for test
    
    * chore: delete old mica
    
    * chore: fix build scripts inaccessible
    
    * chore: exit on .millennium not found
    
    * chore: uncomment database upload

---
+++ scripts/build/upload-dist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "::group::Database"
+
+cd dist
+
+echo "::debug::Building plugin archive..."
+zip -r "$PLUGIN_NAME.zip" .
+
+echo "::debug::Successfully built plugin."
+
+id=$(jq -r '.id' "$PLUGIN_NAME"/metadata.json)
+
+echo "::debug::Uploading plugin to database..."
+gsutil cp "$PLUGIN_NAME.zip" gs://millennium-d9ce0.appspot.com/plugins/"$id.zip"
+echo "::debug::Successfully uploaded plugin to database."
+rm "$PLUGIN_NAME.zip"
+
+echo "::endgroup::"
